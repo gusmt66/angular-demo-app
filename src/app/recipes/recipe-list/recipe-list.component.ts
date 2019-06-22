9555db0232feeea20dce0e5092c537d24e980910
Gustavo Morantes <gmorantes@merricktowle.com>
2019-06-22 13:07:48 -0400

    Adding basic business logic and views.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test','https://storage.needpix.com/rsynced_images/recipe-575434_1280.png'),
+    new Recipe('A Test Recipe', 'This is simply a test','https://storage.needpix.com/rsynced_images/recipe-575434_1280.png')
+  ];
+ 
   constructor() { }
 
   ngOnInit() {
